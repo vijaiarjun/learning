692b3610b10ab2c3d494f913ea18f2e8da4cd9ed
vijaiarjun-somalinga <vijaiarjun.somalinga@ikea.com>
2019-01-22 11:29:45 +0100

    Refactor #5

--- Warehouse/src/test/java/com/training/warehouse/WareHouseApplicationTests.java
+++ Warehouse/src/test/java/com/training/warehouse/WareHouseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.training.TrainingProject;
+package com.training.warehouse;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TrainingProjectApplicationTests {
+public class WareHouseApplicationTests {
 
 	@Test
 	public void contextLoads() {
